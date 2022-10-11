7f181c23445032ec43b99290b092a68e2d63d4e9
ShanthoshPrabhu <shanthoshprabhu30@gmail.com>
2022-10-11 22:50:39 +0530

    new commit

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -17,7 +17,7 @@ export default function login() {
     
     return (
       <>
-        <div className="container mx-auto px-4 h-full">
+        {/* <div className="container mx-auto px-4 h-full">
           <div className="flex content-center items-center justify-center h-full">
             <div className="w-full lg:w-4/12 px-4">
               <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
@@ -90,7 +90,54 @@ export default function login() {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
+        <div className='h-screen flex flex-col  items-center justify-center '>
+          <div className=" flex flex-col break-words mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0 p-10">
+             <form className='w-60'>
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring   w-full ease-linear transition-all duration-150"
+                      placeholder="Email"
+                      value={email}
+                      onChange={e=>setEmail(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Password"
+                      value={password}
+                      onChange={e=>setPassword(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="text-center mt-6">
+                    <button
+                      className="bg-black text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={login}
+                    >
+                      Log in
+                    </button>
+                  </div>
+             </form>
+          </div>
+      </div>
       </>
     );
   }
