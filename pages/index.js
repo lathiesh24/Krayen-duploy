fc4ad82898538e0ab79588b6622255a0b3a8fde7
lathiesh24 <lathumahi24@gmail.com>
2022-11-27 23:29:55 +0530

    recent commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-
-
-import React from 'react'
+import React from "react";
 
 function index() {
-  return (
-    <div></div>
-  )
+  return <div>Hi</div>;
 }
 
-export default index
+export default index;
