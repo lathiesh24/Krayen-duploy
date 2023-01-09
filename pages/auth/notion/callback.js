e3bb8b92130ce27a48a983a1100be3c05f013dc6
ShanthoshPrabhu <shanthoshprabhu30@gmail.com>
2023-01-09 17:46:59 +0530

    new commit!

---
+++ pages/auth/notion/callback.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function callback({response}) {
+    console.log('response',response)
+  return (
+    <div>callback</div>
+  )
+}
+
+export default callback
+
+
+
+export async function getServerSideProps(resolvedUrl){
+    try{
+      const code = resolvedUrl.query.code;
+      console.log('code',resolvedUrl)
+
+      const res = await fetch(`https://api.notion.com/v1/oauth/token`,{
+        method:'post',
+        headers:new Headers({
+            'Authorization':'Basic ' + btoa(process.env.NOTION_CLIENT_ID + ":" + process.env.NOTION_CLIENT_SECRET),
+            'Content-Type':'application/json'
+        }),
+        body:JSON.stringify({
+            grant_type:'authorization_code',
+            code:code,
+            redirect_uri:'http://localhost:3000/auth/notion/callback'
+        })
+      });
+
+      console.log('res',res)
+
+      const response = await res.json()
+
+      return {
+        props:{
+            response
+        }
+      }
+    }catch (err){
+        console.log(err)
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,96 @@
+// import { getDatabase } from "@notionhq/client/build/src/api-endpoints";
+import axios from "axios";
+import Link from "next/link";
 import React from "react";
+import { getBlocks, getDatabase, getPage } from "../library/notion";
 
-function index() {
-  return <div>Hi</div>;
+
+export const databaseId = '4c699e3e758d41248751780fefed7d23';
+export const pageId='4606f5e400c34d68b8a0353328ad0c3c'
+
+export const Text = ({ text }) => {
+  if (!text) {
+    return null;
+  }
+  return text.map((value) => {
+    console.log('value',value)
+    const {
+      annotations: { bold, code, color, italic, strikethrough, underline },
+    
+    } = value;
+    // console.log('text',text)
+    return (
+      <span
+        className={[
+          bold ?  'font-bold': "",
+          code ? ' bg-[#F2F2F2] px-2 py-4 rounded-lg' : "",
+          italic ? ' italic' : "",
+          strikethrough ? ' line-through' : "",
+          underline ? 'underline' : "",
+        ].join(" ")}
+        style={color !== "default" ? { color } : {}}
+      >
+        {value.text.content}
+        {/* {text.content } */}
+        {/* {text.link == null ? text.content : <a href={text.link.url}>{text.content}</a> } */}
+      </span>
+    );
+  });
+};
+function index({posts}) {
+console.log('posts',posts)
+  
+  return (
+    <div className=" w-4/5  flex flex-col pl-96 justify-center pb-40 pt-20 ">
+       
+        
+
+        <h2 className='mb-[70px]'>All Posts</h2>
+       
+          {posts.map((post) => {
+            const date = new Date(post.last_edited_time).toLocaleString(
+              "en-US",
+              {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              }
+            );
+            return (
+              <li key={post.id} className='flex flex-col min-h-[150px]  md:max-w-xl rounded-lg bg-white shadow-lg mb-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:drop-shadow-2xl duration-300 cursor-pointer p-5'>
+                <h3 className=' p-3'>
+                  <Link href={`/${post.id}`}>
+                    <Text text={post.properties.Name.title} />
+                  </Link>
+                </h3>
+                 {/* <p className="w-[400px] h-[150px]">{}</p> */}
+                 <div className="w-[350px] h-[130px] p-3">
+                  <Text text={post.properties.Text.rich_text}/>
+                 </div>
+                <p className=' p-3'>{date}</p>
+                <div className="pl-3">
+                <Link className="" href={`/${post.id}`}>Read post →</Link>
+                </div>
+              </li>
+            );
+          })}
+       
+  
+    </div>
+  )
 }
 
 export default index;
+
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
+  //  const database =await getBlocks(pageId);
+   console.log('dataaaaaa', database);
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  };
+};
