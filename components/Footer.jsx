a71d8a9885ece89e6121fabb49a57742d4fe3374
lathiesh24 <lathumahi24@gmail.com>
2023-01-15 18:47:10 +0530

    footer

---
+++ components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <ul>Company
+            <li><a href="">Twitter</a></li>
+            <li><a href="">Terms</a></li>
+            <li><a href="">Privacy</a></li>
+            <li><a href="">Refund Policy</a></li>
+            <li><a href="">Status</a></li>
+            <li><a href="">Status</a></li>
+        </ul>
+      </div>
+      <div>
+        <ul>Resources
+            <li><a href="">Docs</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Icons generator</a></li>
+            <li><a href="">Gitbook to Notion</a></li>
+            <li><a href="">Showcase</a></li>
+            <li><a href=""> Notion image links tool</a></li>
+        </ul>
+      </div>
+       <div>
+        <ul>Notion templates
+            <li><a href="">Docs template</a></li>
+            <li><a href="">Blog template</a></li>
+            <li><a href="">Changelog template</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
