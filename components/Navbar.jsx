3e908cf267956892aec72c328e7c112a268d1b65
lathiesh24 <lathumahi24@gmail.com>
2023-01-26 23:22:36 +0530

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
        <p className='ml-1 font-medium'>KRAYEN NOTION SITE</p> 
       </div>
       
-      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-3 cursor-pointer md:hidden'>
       <ion-icon name={open ? 'close':'menu'}></ion-icon>
       </div>
 
