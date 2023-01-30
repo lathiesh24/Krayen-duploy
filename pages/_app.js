775cc3dcdd408429592b5792b0e3bed73aa6d019
lathiesh24 <lathumahi24@gmail.com>
2023-01-30 17:57:50 +0530

    recent

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import 'react-notion-x/src/styles.css'
-import Head from 'next/head'
-
-
+import "react-notion-x/src/styles.css";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-     <Head>
+      <Navbar />
+      <Head>
         <script src="https://www.youtube.com/iframe_api"></script>
       </Head>
-    <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
-  )
-  
+  );
 }
 
-export default MyApp
+export default MyApp;
